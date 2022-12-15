de973a50fbebac0701e68fb86f9aacc694b8c374
yzamir <yzamir@redhat.com>
2022-12-15 16:19:08 +0200

    initial-commit
    Signed-off-by: yzamir <yzamir@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
+[![npm version](https://badge.fury.io/js/yaindexer.svg)](https://badge.fury.io/js/yaindexer)
+
 # yaindexer
 Yet another typescript indexer
+
+yaindexer is a tool that create index files for typescript and javascrpt code.
+
+## Usage
+
+Use a source code directory without index files, and add automatically generated index files.
+
+``` bash
+npm install --location=global yaindexer
+
+# add npm bin path to your PATH, or use full
+# excutable path, e.g. $(npm bin --location=global)/crdtoapi
+yaindexer --help
+
+# add index files for your project
+yaindexer -i ./src
+
+# add index files for your project and overwrite the current index files
+# with new version
+yaindexer --overwrite -i ./src
+```
+
+## Build
+
+Run this scripts to lint and publish the package.
+
+``` bash
+npm install
+npm run lint:fix
+npm run build
+npm publish
+```
---
+++ src/index.ts
@@ -0,0 +1,154 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { writeFile, readdir, lstat } from 'fs/promises';
+import { existsSync } from 'fs';
+import path from 'path';
+import Mustache from 'mustache';
+
+let regexp: RegExp;
+let skipRegexp: RegExp;
+
+const DEFAULT_OUTPUT = 'index.ts';
+const DEFAULT_COMMENT = '// Auto generated index file.';
+const DEFAULT_REGEXP = '^(?!.*test\.ts[x]?).*\.ts[x]?$';
+const DEFAULT_SKIP_REGEXP = '';
+const DEFAULT_EXPORT_LINE_TEMPLATE = "export * from './{{name}}';\n"
+
+/**
+ * Define the CLI options
+ */
+const program = new Command();
+program
+  .version("0.0.1")
+  .description("Create index files")
+  .option("-i, --in <dir>", "Input directory path - required")
+  .option("--output <filename>", "Output filename")
+  .option("--template <text>", "Template for export line")
+  .option("--overwrite", "Overwrite existing index files")
+  .option("-d, --exportDirectories", "Export directoroies in the index files")
+  .option("-r, --regexp <regexp>", "Index files matching regexp")
+  .option("-s, --skipRegexp <regexp>", "Skip files matching regexp")
+  .option("-c, --comment <text>", "Add comment on top of index file")
+  .parse(process.argv);
+
+const options = program.opts();
+
+// Ceck for required fields
+if (!options.in) {
+    console.log("error: missing mandatory argument --in");
+    process.exit(1);
+}
+
+// Set default values
+if (!options.output) {
+    options.output = DEFAULT_OUTPUT;
+}
+
+if (!options.template) {
+    options.template = DEFAULT_EXPORT_LINE_TEMPLATE;
+}
+
+if (!options.comment) {
+    options.comment = DEFAULT_COMMENT;
+}
+
+if (!options.exportDirectories) {
+    options.exportDirectories = false;
+}
+
+if (options.regexp === undefined) {
+    options.regexp = DEFAULT_REGEXP;
+}
+
+if (options.skipRegexp === undefined) {
+    options.skipRegexp = DEFAULT_SKIP_REGEXP;
+}
+
+try {
+    regexp = new RegExp(options.regexp);
+    skipRegexp = new RegExp(options.skipRegexp);
+} catch (error) {
+    console.log(`error occurr parsing regular expression (${error})`);
+    process.exit(1);
+}
+
+/**
+ * Recursivly read all code files in a directory
+ * and create index file if indexable file found
+ * 
+ * @param dirPath is the directory to read
+ */
+const readSrcDir = async (dirPath: string) => {
+    let fileTxt = "";
+    const indexFile = path.join(dirPath, options.output);
+    const indexFileExists = existsSync(indexFile);
+
+    try {
+        const files = await readdir(dirPath);    
+        for (const file of files) {
+            // Check file stats
+            const filePath = path.join(dirPath, file);
+            const ext = path.extname(file);
+            const name = path.basename(file, ext);
+
+            const stat = await lstat(filePath);
+            const isDir = stat.isDirectory();
+            const isIndex = file === 'index.js' || file === 'index.ts';
+
+            // Do we want to export this file/dir ?
+            const exportFile =
+                !isIndex &&
+                (options.exportDirectories || !isDir) &&
+                (!options.regexp || regexp.test(filePath)) &&
+                (!options.skipRegexp || !skipRegexp.test(filePath));
+
+            // Export file if matching regexps
+            if (exportFile) {
+                const templateData = {
+                    dir: dirPath,
+                    file: file,
+                    isDir: isDir,
+                    ext: ext,
+                    name: name,
+                }
+
+                fileTxt = fileTxt + Mustache.render(options.template, templateData);
+            }
+
+            // Index sub directories
+            if (isDir) {
+                await readSrcDir(filePath);
+            }
+        }
+
+        // Create index file if found an indexable src file
+        if ((!indexFileExists || options.overwrite) && fileTxt !== "") {
+            fileTxt = options.comment + '\n\n' + fileTxt;
+
+            writeFile(indexFile, fileTxt);
+        }
+    } catch (error) {
+       console.log(`error occurr ed while reading the input directory (${error})`);
+       process.exit(1);
+    }
+};
+
+/**
+ * Create an Index files from a directory
+ */
+const createIndexFiles = async () => {
+    const data = await readSrcDir(options.in);
+};
+
+console.log('Indexing options:')
+for (let opt in options) {
+    console.log(`  ${opt}: ${options[opt]}`);
+}
+
+console.log('')
+console.log('Running...')
+
+createIndexFiles().then(() => { 
+    console.log('Done.')
+});
